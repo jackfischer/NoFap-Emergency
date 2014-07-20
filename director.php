dd879b837ea563d0e723b3896dd71c0491660468
Jack Fischer <jfische8@binghamton.edu>
2014-07-20 13:52:42 -0400

    improved front end/responsive, redirect working with PHP

--- director.php
+++ director.php
@@ -1,3 +1,6 @@
 <?php
+if ($_GET["cat"] == "emergency")
+	echo "http://reddit.com/r/nofap";
+else
 	echo "http://google.com";
 ?>
---
+++ jquery.jump.js
@@ -0,0 +1,22 @@
+$(emergency).click(function() {
+	  $.get("director.php", { cat:"emergency" }, function(data) {
+		      window.location.href = data;
+		        });
+});
+$(depression).click(function() {
+	  $.get("director.php", { cat:"depression" }, function(data) {
+		      window.location.href = data;
+		        });
+});
+$(rejection).click(function() {
+	  $.get("director.php", { cat:"rejection" }, function(data) {
+		      window.location.href = data;
+		        });
+});
+$(relapsed).click(function() {
+	  $.get("director.php", { cat:"relapsed" }, function(data) {
+		      window.location.href = data;
+		        });
+});
+
+
