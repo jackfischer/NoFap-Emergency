3a45fdee7c328562fd5c21746f8930a820a8e6dd
Jack Fischer <jfische8@binghamton.edu>
2014-09-11 11:16:36 -0400

    footer, db, ios icon, GA on bookmarklet, inlining js, dont even remember

--- director.php
+++ director.php
@@ -30,12 +30,13 @@ echo mysql_fetch_object($result)->link;
 }
 
 if ($_GET["cat"] == "bookmarklet") {
-$query='select link from posts where cat=\'em\' order by RAND() limit 1';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
 echo "<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">";
 
 echo "<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-53301604-1', 'auto'); ga('send', 'pageview'); </script>";
- 
+
+$query='select link from posts where cat=\'em\' order by RAND() limit 1';
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
 echo "<script> window.location.href=\"".mysql_fetch_object($result)->link."\";</script>";
 }
 
