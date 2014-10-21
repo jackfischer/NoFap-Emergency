cfbc589a5954f208949c3dd168d879085a3b23b0
Jack Fischer <jfische8@binghamton.edu>
2014-10-21 12:57:25 -0400

    API rebuilt. PDO and full religion support

--- director.php
+++ director.php
@@ -1,77 +1,51 @@
 <?php
 
-graphdat_begin("ServeAPI");
-
 include 'credentials.php';
-
-mysql_connect($host, $user, $password) or die('Could not connect: ' . mysql_error());
-mysql_select_db('posts') or die('Could not select db');
-
-if ($_GET["cat"] == "emergency") {
-$query='select link from posts where cat=\'em\' order by RAND() limit 1';
-
-$insert='INSERT INTO events (id, cat, timestamp) VALUES (NULL, "emergency", CURRENT_TIMESTAMP)';
-$insertresult = mysql_query($insert) or die('Query failed: ' . mysql_error());
-
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-header("Access-Control-Allow-Origin: *");
-
-echo mysql_fetch_object($result)->link;
+$cat = $_GET["cat"];
+try {
+	$DBH = new PDO("mysql:host=localhost;dbname=posts", $user, $password);
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch(PDOException $e) {
+	echo $e->getMessage(); 
 }
 
-if ($_GET["cat"] == "rejection") {
-$query='select link from posts where cat=\'rej\' order by RAND() limit 1';
+if ($cat != "bookmarklet") {
+	if($_GET["religious"] == "true") {
+		$query = "select link from links where cat=:cat order by RAND() limit 1";
+		$insert = "insert into events (id, cat, timestamp, religious) values (NULL, :cat, CURRENT_TIMESTAMP, 1)";
+	}
+	else {
+		$query = "select link from links where cat=:cat and religious is NULL order by RAND() limit 1";
+		$insert = "insert into events (id, cat, timestamp) values (NULL, :cat, CURRENT_TIMESTAMP)";
+	}
 
-$insert='INSERT INTO events (id, cat, timestamp) VALUES (NULL, "rejection", CURRENT_TIMESTAMP)';
-$insertresult = mysql_query($insert) or die('Query failed: ' . mysql_error());
+	$IH = $DBH->prepare($insert);
+	$IH->execute(array(":cat" => $cat));
 
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+	$STH = $DBH->prepare($query);
+	$STH->execute(array(":cat" => $cat));
 
-header("Access-Control-Allow-Origin: *");
-
-echo mysql_fetch_object($result)->link;
+	header("Access-Control-Allow-Origin: *");
+	echo $STH->fetch()[0];
 }
 
-if ($_GET["cat"] == "depression") {
-$query='select link from posts where cat=\'dep\' order by RAND() limit 1';
 
-$insert='INSERT INTO events (id, cat, timestamp) VALUES (NULL, "depression", CURRENT_TIMESTAMP)';
-$insertresult = mysql_query($insert) or die('Query failed: ' . mysql_error());
+if ($cat == "bookmarklet") {
+	
+	echo "<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">";
+	echo "<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-53301604-1', 'auto'); ga('require', 'displayfeatures'); ga('send', 'pageview'); </script>";
 
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+	$insert="INSERT INTO events (id, cat, timestamp) VALUES (NULL, \"bkmarklet\", CURRENT_TIMESTAMP)";
+	$IH = $DBH->prepare($insert);
+	$IH->execute();
 
-header("Access-Control-Allow-Origin: *");
+	$query="select link from links where cat=:cat order by RAND() limit 1";
+	$STH = $DBH->prepare($query);
+	$STH->execute(array(":cat" => "em"));
+	$result = $STH->fetch()[0];
 
-echo mysql_fetch_object($result)->link;
+	echo "<script> window.location.href=\"".$result."\";</script>";
 }
-
-if ($_GET["cat"] == "relapsed") {
-$query='select link from posts where cat=\'rel\' order by RAND() limit 1';
-
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-$insert='INSERT INTO events (id, cat, timestamp) VALUES (NULL, "relapsed", CURRENT_TIMESTAMP)';
-$insertresult = mysql_query($insert) or die('Query failed: ' . mysql_error());
-
-header("Access-Control-Allow-Origin: *");
-
-echo mysql_fetch_object($result)->link;
-}
-
-if ($_GET["cat"] == "bookmarklet") {
-echo "<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">";
-echo "<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-53301604-1', 'auto'); ga('require', 'displayfeatures'); ga('send', 'pageview'); </script>";
-
-$insert='INSERT INTO events (id, cat, timestamp) VALUES (NULL, "bkmarklet", CURRENT_TIMESTAMP)';
-$insertresult = mysql_query($insert) or die('Query failed: ' . mysql_error());
-
-$query='select link from posts where cat=\'em\' order by RAND() limit 1';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-
-echo "<script> window.location.href=\"".mysql_fetch_object($result)->link."\";</script>";
-}
-
-graphdat_end("ServeAPI");
 
 ?>
