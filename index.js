17ea59177258dac671c29a67cbc989038ea7235d
Jack Fischer <jack.fischer11@gmail.com>
2018-01-12 22:25:02 -0500

    added /religious feature, began cleaning legacy files, refreshed front end dependencies

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const {Link, Suggestion, Log} = require('./db')
 const express = require('express')
-const cloudflare = require('cloudflare-express');
-const Recaptcha = require('express-recaptcha');
-const {recaptcha_secret, raven_dsn} = require('./secrets')
-const Raven = require('raven');
+const cloudflare = require('cloudflare-express')
+const Recaptcha = require('express-recaptcha')
+const {recaptcha_secret, recaptcha_id, raven_dsn} = require('./secrets')
+const Raven = require('raven')
 
 // Load in-memory copy of links
-const categories = ['em', 'rel', 'dep', 'rej'];
+const categories = ['em', 'rel', 'dep', 'rej']
 const cat_o = {}
-categories.forEach(cat => cat_o[cat] = []);
-const linkcache = {true: cat_o, false: {...cat_o}};
+categories.forEach(cat => cat_o[cat] = [])
+const linkcache = {true: cat_o, false: {...cat_o}}
 Link.findAll({where: {down: false}}).then(rows => {
   rows.forEach((row) => {
     categories.forEach(category => {
       Array.from(Array(row[category])).forEach(() => {
         linkcache[row.religious][category].push(row.url)
-      });
-    });
-  });
-  console.log("Loaded link cache successfully")
-});
+      })
+    })
+  })
+  console.log('Loaded link cache successfully')
+})
 //TODO trigger to reload upon pg modification
 
 
 const app = express()
-Raven.config(raven_dsn).install();
-app.use(Raven.requestHandler());
+Raven.config(raven_dsn).install()
+app.use(Raven.requestHandler())
 app.use(express.static('static'))
-app.use(cloudflare.restore());
+app.use(cloudflare.restore())
 
 
 function nocache(req, res, next) {
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
-  next();
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+  res.header('Expires', '-1')
+  res.header('Pragma', 'no-cache')
+  next()
 }
 
 function getlinkdata(req) {
@@ -47,11 +47,11 @@ function getlinkdata(req) {
 
 
 app.get('/director.php', nocache, function(req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*')
   response_data = getlinkdata(req)
   res.send(response_data.url)
   Log.create(response_data) //TODO append to an in memory queue instead
-});
+})
 
 app.get('/mobileapi.php', nocache, function (req, res) {
   response_data = getlinkdata(req)
@@ -59,35 +59,33 @@ app.get('/mobileapi.php', nocache, function (req, res) {
   Log.create(response_data)
 })
 
-const recaptcha = new Recaptcha("6LcboPoSAAAAAIH6t90C3ppYFQUnNLdvZwGc-eA3", recaptcha_secret);
+const recaptcha = new Recaptcha(recaptcha_id, recaptcha_secret)
 app.get('/thankyou.php', nocache, recaptcha.middleware.verify, function(req, res) {
   if (!req.recaptcha.error) {
     Suggestion.create({
       url: req.query.link, comment: req.query.message, ip: req.cf_ip
-    });
-    res.json({"status":"success"})
+    })
+    res.json({'status':'success'})
   } else {
-    res.status(401).json({"status": "error"})
+    res.status(401).json({'status': 'error'})
   }
-});
+})
 
+app.get('/religious', function(req,res) {
+  res.cookie('religious', 'true', {expires: new Date(Date.now() + 900000)})
+  res.redirect('/')
+})
 
 app.get('/helloworld', function(req,res) {
-  res.send("hello world");
+  res.send('hello world')
 })
 app.get('/error', function(req,res) {
-  throw new Error('Broke!');
-});
+  throw new Error('Broke!')
+})
 
 
-app.use(Raven.errorHandler());
+app.use(Raven.errorHandler())
 app.listen(3000, () => console.log('App is listening'))
-//TODO do logging correctly
-
-
-
-
-
 
 
 
