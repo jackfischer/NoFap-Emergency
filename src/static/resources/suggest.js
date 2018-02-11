e59eb60efb0fd76f14e042e1cea4dc22c77f9030
Jack Fischer <jack.fischer11@gmail.com>
2018-02-11 13:17:29 -0500

    new sequelize and pg7! js for suggestor

--- src/static/resources/suggest.js
+++ src/static/resources/suggest.js
@@ -6,3 +6,15 @@ function validateForm() {
   }
 }
 
+function send() {
+  $("button[name=submit]").disabled = true
+  $.post("thankyou.php", {
+    link: $('input[name="link"]').value,
+    message: $('input[name="message"]').value,
+    g-recaptcha-response: grecaptcha.getResponse()
+  }, function() {alert("success!")})
+  .fail(function() {
+    $("button[name=submit]").disabled = false
+    alert("didnt work")
+  })
+}
