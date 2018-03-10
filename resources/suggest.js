03e7cd2205148fad6e2ffc0c0deca70354f8f9d6
Jack Fischer <jack.fischer11@gmail.com>
2018-03-10 17:23:41 -0500

    new suggestor setup

--- resources/suggest.js
+++ resources/suggest.js
@@ -1,20 +1,14 @@
+if (location.href.indexOf("success") > 0) {
+  alert("Thank you for your submission!");
+}
+if (location.href.indexOf("failed") > 0) {
+  alert("Something went wrong with your submission.");
+}
+
 function validateForm() {
   var x = document.forms["suggestor"]["link"].value;
   if (x == null || x == "") {
     alert("Form must be filled out");
     return false;
   }
-}
-
-function send() {
-  $("button[name=submit]").disabled = true
-  $.post("thankyou.php", {
-    link: $('input[name="link"]').value,
-    message: $('input[name="message"]').value,
-    g-recaptcha-response: grecaptcha.getResponse()
-  }, function() {alert("success!")})
-  .fail(function() {
-    $("button[name=submit]").disabled = false
-    alert("didnt work")
-  })
 }
